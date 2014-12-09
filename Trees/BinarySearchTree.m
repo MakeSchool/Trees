03227a5e413b2b0a945581cf7093df5b151a0d2c
Daniel Haaser <thedapperdan@gmail.com>
2014-12-09 01:26:02 -0800

    objectForKey, addObject

--- Trees/BinarySearchTree.m
+++ Trees/BinarySearchTree.m
@@ -18,6 +18,36 @@
 
 @implementation BinarySearchTree
 
+- (id)objectForKey:(NSString *)key
+{
+    Node* node = [self recursivelyFindNodeForKey:key withCurrentNode:self.rootNode];
+    
+    if (node)
+    {
+        return node.data;
+    }
+    else
+    {
+        return nil;
+    }
+}
+
+- (void)addObject:(id)object forKey:(NSString *)key
+{
+    Node* newNode = [Node new];
+    newNode.data = object;
+    newNode.key = key;
+    
+    if (!self.rootNode)
+    {
+        newNode.parent = nil;
+        self.rootNode = newNode;
+    }
+    else
+    {
+        [self recursivelyAddNode:newNode withCurrentNode:self.rootNode];
+    }
+}
 
 
 @end
